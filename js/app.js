5d3c4aa83c24ad269052fbf3d2d1df988e8f873d
Zenox2003 <smith.lindley93313@gmail.com>
2024-07-15 15:04:10 -0400

    functions added to handle board

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 /*-------------------------------- Constants --------------------------------*/
 
-
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 
 /*---------------------------- Variables (state) ----------------------------*/
 let board, turn, winner, tie
@@ -15,7 +24,7 @@ const messageEl = document.getElementById(`message`)
 init()
 
 function init() {
-    board = [``, `X`, ``, ``, `O`, ``, ``, ``, ``]
+    board = [``, ``, ``, ``, ``, ``, ``, ``, ``]
     turn = `X`
     winner = false
     tie = false
@@ -24,6 +33,7 @@ function init() {
 
 function render() {
     updateBoard()
+    updateMessage()
 }
 
 function updateBoard() {
@@ -42,10 +52,35 @@ function updateBoard() {
     });
 }
 
+function updateMessage () {
+    if (!winner && !tie) {
+        messageEl.textContent = `It is ${turn}'s turn`
+    } else if (!winner && tie) {
+        messageEl.textContent = "Outplayed by each other!"
+    } else {
+        messageEl.textContent = `${turn} wins the game`
+    }
+}
+
+function handleClick(evt) {
+const squareIndex = evt.target.id
+if(board[squareIndex] === `X` || board[squareIndex] === `O` || winner)
+    {
+    return
+    }
+    placePiece(squareIndex)
+    console.log(board);
+}
+
+function placePiece(index) {
+    board[index] = turn
+}
+
 
 /*----------------------------- Event Listeners -----------------------------*/
-
-
+squareEls.forEach((squareEl) => {
+    squareEl.addEventListener(`click`, handleClick)
+})
 
 //1) Define the required variables used to track the state of the game.
 
